ff23ed4e0a76e1da3e13fc03d6ae6fafe2e4e32d
MrBitasmblTester <21100302@ibsu.edu.ge>
2025-11-04 12:40:32 +0400

    Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// src/App.tsx
+import React from 'react';
+import LessonList from './components/LessonList';
+const App = () => (
+  <div>
+    <h1>CodeQuest Dashboard</h1>
+    <LessonList />
+  </div>
+);
+export default App;
